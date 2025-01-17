a35ef2ed57be2b829dab97c106381d43f109514a
GrzeCHu-a11y <grzegorzgonczowski9@gmail.com>
2025-01-17 12:16:14 +0100

    feat: Created header at home page
    - Created header.
    - Global h1 style.
    - Local p style.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,10 @@ import styles from "./Home.module.css";
 const Home = () => {
     return (
         <section className={styles.section}>
-            <h1>Test</h1>
+            <header className={styles.header}>
+                <h1 className={styles.h1}>Sprawdź, które składniki zatykają pory</h1>
+                <p className={styles.p}>Dowiedz się, które składniki kosmetyczne mogą zatykać pory i powodować niedoskonałości skóry.</p>
+            </header>
         </section>
     );
 };
