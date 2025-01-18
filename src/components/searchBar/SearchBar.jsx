e5cf445e8929c510716c6972558f44a7e2844a9f
GrzeCHu-a11y <grzegorzgonczowski9@gmail.com>
2025-01-18 22:32:59 +0100

    style: responisve modification
    - added responisve for searchBar and Home
    - typography modification.
    - colors modification.
    - cosmetics.jpg was compressed.

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -3,7 +3,7 @@ import styles from "./SearchBar.module.css";
 const SearchBar = () => {
     return (
         <div className={styles.wrapper}>
-            <input className={styles.SearchBar} type="search" name="" id="" placeholder="Wklej tu składniki produktu" />
+            <input className={styles.SearchBar} type="search" name="" id="" placeholder="Wklej tu składniki produktu..." />
             <span className={styles.fa}><a href="#"><i className="fas fa-search"></i></a></span>
         </div>
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,16 @@ import styles from "./Home.module.css";
 
 const Home = () => {
     return (
-        <section className={styles.section}>
-            <header className={styles.header}>
-                <h1 className={styles.h1}>Sprawdź, które składniki zatykają pory</h1>
-                <p className={styles.p}>Dowiedz się, które składniki kosmetyczne mogą zatykać pory i powodować niedoskonałości skóry.</p>
-            </header>
-            <SearchBar />
-        </section>
+        <>
+            <section className={styles.section}>
+                <header className={styles.header}>
+                    <h1 className={styles.h1}>Sprawdź, które składniki zatykają pory</h1>
+                    <p className={styles.p}>Dowiedz się, które składniki kosmetyczne mogą zatykać pory i powodować niedoskonałości skóry.</p>
+                </header>
+                <SearchBar />
+            </section>
+        </>
+
     );
 };
 
