84989d88fb4e62373b1b4d923feae31fca87444e
GrzeCHu-a11y <grzegorzgonczowski9@gmail.com>
2025-01-19 11:38:46 +0100

    refactor: Typography update
    - Deleted poppins font.
    - Creted responsive typography.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import styles from "./Home.module.css";
 const Home = () => {
     return (
         <>
-            <section className={styles.section}>
+            <section className={styles.firstSection}>
                 <header className={styles.header}>
                     <h1 className={styles.h1}>Sprawdź, które składniki zatykają pory</h1>
                     <p className={styles.p}>Dowiedz się, które składniki kosmetyczne mogą zatykać pory i powodować niedoskonałości skóry.</p>
@@ -12,7 +12,6 @@ const Home = () => {
                 <SearchBar />
             </section>
         </>
-
     );
 };
 
